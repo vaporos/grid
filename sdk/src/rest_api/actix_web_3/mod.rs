5053e93f0614177f62e9aa5e07d3b85a0eb4ce9c
Shawn T. Amundson <amundson@bitwise.io>
2021-02-01 16:22:42 -0600

    Add crate::rest_api::actix_web_3 module
    The current implementation of the REST API uses Actix Web 3. The pieces
    which rely on Actix Web code will be placed in the actix_web_3 module.
    
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

--- sdk/src/rest_api/actix_web_3/mod.rs
+++ sdk/src/rest_api/actix_web_3/mod.rs
@@ -11,6 +11,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-#[cfg(feature = "rest-api-actix-web-3")]
-mod actix_web_3;
